9184c2ab990208ad8661c22dea2a7ad1900e5aca
Garumuni Omalka Nidarshana <omalkanidarshana@gmail.com>
2026-06-11 01:08:31 +0530

    db cred changes

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_NAME', 'anovex_admin');
-define('DB_USER', 'root');          // change to your MySQL user
+define('DB_USER', 'anovex_admin');          // change to your MySQL user
 define('DB_PASS', 'admin@anovex2026');              // change to your MySQL password
 define('DB_CHARSET', 'utf8mb4');
 
